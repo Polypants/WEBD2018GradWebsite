475bb70c07cb9084d1050b81cfaf1de038564c46
Ditto <Ditto@Pauls-MacBook-Pro.local>
2018-02-14 23:29:37 -0500

    removed extra files and set up example router setup

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <h1>An example About page</h1>
+    );
+  }
+}
+
+export default About;
