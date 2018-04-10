951b00f2d46fc8bfd229daa01a4f1360b22cd69f
Richard5277 <richard.dev@icloud.com>
2018-04-09 20:15:01 -0400

    add google map

--- src/Event/Event.js
+++ src/Event/Event.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
 import './Event.css';
 
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+const greyStyleMap = require("./GreyStyleMap.json");
+// const { MarkerWithLabel } = require("react-google-maps/lib/components/addons/MarkerWithLabel");
+
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDqWPXhLzG-Br3bXIN5T5gTZyiRqdRFvvI&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `32vh`, width:`100%` }} />,
+    containerElement: <div className='googleMapWrapper' />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) =>
+  <GoogleMap
+    defaultZoom={16}
+    defaultCenter={{ lat: 43.647100, lng: -79.403348 }}
+    defaultOptions={{ styles: greyStyleMap }}
+    mapTypeControl={false}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: 43.647100, lng: -79.403348 }} />}
+  </GoogleMap>
+);
+
+
 class Event extends Component {
+
+  componentDidMount(){
+
+  }
+
   render() {
     return (
       <div className="Event">
@@ -17,7 +49,7 @@ class Event extends Component {
               <p>The Burroughes Building<br/>639 Queen St.W, Toronto, ON<br/>M5V 2B7</p>
             </div>
           </div>
-          <div className="Event_Item Event_Item_3">
+          <div className="Event_item Event_Item_3">
             <div className="text_container_3">
               <h3 className="Event_title">WHEN</h3>
               <p>April 25th 2018<br/>5pm - 8pm</p>
@@ -25,6 +57,14 @@ class Event extends Component {
           </div>
         </div>
       <div className="Event_reservation" id="GoogleMap">
+        <MyMapComponent
+          isMarkerShown
+          mapTypeControl={false}
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `32vh`, width:`100%` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
         <div className="text_container_4">
           <h2 className="Event_mapTitle">WE CAN'T WAIT TO MEET YOU!</h2>
           <div className="Event_rsvpButton">RSVP Now</div>
